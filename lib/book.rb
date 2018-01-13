906461144f99924b36f365b6df07c2c4bd785d06
Maria Balsamo <mia.balsamo@gmail.com>
2018-01-13 18:25:37 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -50,10 +50,11 @@
 
 class Book
 
-  attr_accessor :title
+  attr_accessor :title, :author
 
-  def initialize(title)
+  def initialize(title, author)
     @title = title
+    @author = author
   end
 
 end
